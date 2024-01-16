f974fd2d4b0ba91e9c7b2bff7dddd762a2cead5e
affaf04 <affaftaouti@gmail.com>
2024-01-16 12:22:35 -0500

    edit

---
+++ src/pages/Products/ProductsListItem.jsx
@@ -0,0 +1,15 @@
+
+export default function ProductsListItem({ menuItem, handleAddToOrder }) {
+    return (
+      <div className={styles.MenuListItem}>
+        <div className={styles.emoji + ' ' + 'flex-ctr-ctr'}>{menuItem.emoji}</div>
+        <div className={styles.name}>{menuItem.name}</div>
+        <div className={styles.buy}>
+          <span>${menuItem.price.toFixed(2)}</span>
+          <button className="btn-sm" onClick={() => handleAddToOrder(menuItem._id)}>
+            ADD
+          </button>
+        </div>
+      </div>
+    );
+  }
