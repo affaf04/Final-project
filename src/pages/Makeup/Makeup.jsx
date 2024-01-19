f3d314a04faf914250dca414e4388f39220095ed
affaf04 <affaftaouti@gmail.com>
2024-01-19 14:18:19 -0500

    adding api

--- src/pages/Makeup/Makeup.jsx
+++ src/pages/Makeup/Makeup.jsx
@@ -10,6 +10,7 @@ const Makeup = () => {
   const fetchProducts = async () => {
     try {
       const response = await fetch(
+        `http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=${keyword}`
       )
 
       const data = await response.json();
